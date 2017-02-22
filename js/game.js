cf4cb007d721d3dac50d7103ffda14a479060530
Jamie Sykes <jaymehsykes@gmail.com>
2017-02-22 22:24:17 +0000

    Add in window scaling and new comments

--- js/game.js
+++ js/game.js
@@ -1,28 +1,40 @@
 window.onload = function() {
-	let renderer = PIXI.autoDetectRenderer(256, 256, { antialias: false, transparent: false, resolution: 1 });
+	// Loads in a new render at a resolution of 1 with a few default options
+	let renderer = PIXI.autoDetectRenderer(
+		256, 256, { antialias: false, transparent: false, resolution: 4 }
+	);
 
 	//Add the canvas to the HTML document
 	document.body.appendChild(renderer.view);
 
 	//Create a container object called the `stage`
 	let stage = new PIXI.Container();
+	let scale = scaleToWindow(renderer.view);
 
 	//Tell the `renderer` to `render` the `stage`
 	renderer.render(stage);
 
+	// Add an image to the loader
 	PIXI.loader
 	  .add("/img/snake-item.png")
 	  .load(setup);
 
-	console.log(PIXI.loader);
+	// Rescales the window on resize and ensures
+	// everything is central
+	window.addEventListener("resize", event => {
+		scaleToWindow(renderer.view);
+	});
 
+	// Setup function to run once load completed sucessfully
 	function setup() {
-	  let sprite = new PIXI.Sprite.fromImage("/img/snake-item.png");
+		// Load in image and assign it to a sprite
+	  	let sprite = new PIXI.Sprite.fromImage("/img/snake-item.png");
 
-	  stage.addChild(sprite);
+	  	// Add the sprite to the stage
+	  	stage.addChild(sprite);
 
-	  renderer.render(stage);
-
-	  console.log(sprite);
+	  	// Render the stage so we can see the sprite
+	  	renderer.render(stage);
 	}
 }
+
